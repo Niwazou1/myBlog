f6829c0866b00cdf8e48bb26686884d56b107ecc
akasaMac <tarou_17n1101159@nnn.ed.jp>
2017-07-14 16:09:21 +0900

    Implement redirect

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  r.send('Hello World!');
-});
-app.get('/name', function(req, res){
-    res.send('Akasa');
+app.get('', function(req, r){
+  r.send('Hello');
 });
 
-app.get('/show/:id', function(req, res){
-    res.send("ID:" + req.params.id);
+app.get('/', function (req, r) {
+  r.redirect(302, '/name');
 });
+app.get('/name', function(req, r){
+    r.send('Akasa');
+});
+
+app.get('/show/:id', function(req, r){
+    r.send("ID:" + req.params.id);
+});
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
